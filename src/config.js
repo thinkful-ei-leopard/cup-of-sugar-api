6054769128602fbcd90f684b472e6b20827bfe5d
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-20 15:22:02 -0600

    working on dependencies

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8080,
+    PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/snapshot',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/snapshot_test',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/cup-of-sugar',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/cup-of-sugar-test',
     JWT_SECRET: process.env.JWT_SECRET || 'its a secret',
   }
