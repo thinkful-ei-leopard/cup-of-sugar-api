5648924eddc88092bc783f90b2bfc916924e6c51
wavinginspace <paulbaisley@gmail.com>
2020-04-21 11:11:41 -0400

    fixed post router import

--- src/app.js
+++ src/app.js
@@ -2,39 +2,37 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const helmet=require('helmet');
+const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const authRouter = require('./auth/auth-router')
-const usersRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router');
+const usersRouter = require('./users/users-router');
+const postsRouter = require('./posts/posts-router');
 
-const app= express();
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
 app.get('/', (req, res) => {
-     res
-        .status(200)
-        .send('Hello, world!')
+  res.status(200).send('Hello, world!');
 });
 
-app.use('/api/users', usersRouter)
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/posts', postsRouter);
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-      response = { error: { message: 'server error' } }
-    } else {
-      console.error(error)
-      response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-     })
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
 module.exports = app;
--- src/posts/posts-router.js
+++ src/posts/posts-router.js
@@ -1,80 +1,88 @@
-const express = require('express')
-const PostsService = require('./posts-service')
-const { requireAuth } = require('../middleware/jwt-auth')
-const path = require('path')
+const express = require('express');
+const PostsService = require('./posts-service');
+const { requireAuth } = require('../middleware/jwt-auth');
+const path = require('path');
 
-const postsRouter = express.Router()
-const jsonBodyParser = express.json()
-
-postsRouter
-    .route('/')
-    .get(requireAuth, (req, res, next) => {
-        const posts = await PostsService.getPostsByZip(req.app.get('db'), req.user.zip)
-                if(!posts) {
-                    res
-                        .status(404)
-                        .send({ error: {message: 'No posts found'}})
-                }
-                res
-                    .status(200)
-                    .json(posts)
-            .catch(next)
-    })
-    .post(requireAuth, jsonBodyParser, async (req, res, next) => {
-        const { type, title, comments, description } = req.body
-        const user_id = req.user.id
-        const zip = req.user.zip
-        const user_name = req.user.name
-        const user_userName = req.user.userName
-        const newPost = { user_id, user_name, user_userName, zip, type, title, comments, description }
-        if(!type) {
-            return res
-                .status(400)
-                .json({ error: {message: 'Type required'}})
-        }
-        if(!title) {
-            return res
-                .status(400)
-                .json({ error: {message: 'Title required'}})
-        }
-        if(!description) {
-            return res
-                .status(400)
-                .json({ error: {message: 'Description required'}})
-        }
-        if(description.length > 500) {
-            return res
-                .status(400)
-                .json({error: {message: 'Description must not exceed 500 characters'}})
-        }
-        if(title.length > 60) {
-            return res
-                .status(400)
-                .json({error: {message: 'Title must not exceed 60 characters'}})
-        }
-        try {
-        const post = await PostsService.insertPost(req.app.get('db'), newPost) 
-            res
-                .status(201)
-                .location(path.posix.join(req.originalUrl, `${post.id}`))
-                .json(post)
-        }
-        catch{next}
-    })
+const postsRouter = express.Router();
+const jsonBodyParser = express.json();
 
 postsRouter
-    .route('/:post_id')
-    .delete(requireAuth, async (req, res, next) => {
-        try {
-        const post = await PostsService.deletePost(req.app.get('db'), req.params.post_id)
-                if(!post) {
-                    return res.status(404).json({
-                        error: {message: 'Post does not exist'}
-                    })
-                }
-                res
-                    .status(204)
-                    .end()
-        }
-        catch{next}
-    })
+  .route('/')
+  .get(requireAuth, async (req, res, next) => {
+    const posts = await PostsService.getPostsByZip(
+      req.app.get('db'),
+      req.user.zip
+    );
+    if (!posts) {
+      res.status(404).send({ error: { message: 'No posts found' } });
+    }
+    res.status(200).json(posts).catch(next);
+  })
+  .post(requireAuth, jsonBodyParser, async (req, res, next) => {
+    const { type, title, comments, description } = req.body;
+    const user_id = req.user.id;
+    const zip = req.user.zip;
+    const user_name = req.user.name;
+    const user_userName = req.user.userName;
+    const newPost = {
+      user_id,
+      user_name,
+      user_userName,
+      zip,
+      type,
+      title,
+      comments,
+      description,
+    };
+    if (!type) {
+      return res.status(400).json({ error: { message: 'Type required' } });
+    }
+    if (!title) {
+      return res.status(400).json({ error: { message: 'Title required' } });
+    }
+    if (!description) {
+      return res
+        .status(400)
+        .json({ error: { message: 'Description required' } });
+    }
+    if (description.length > 500) {
+      return res
+        .status(400)
+        .json({
+          error: { message: 'Description must not exceed 500 characters' },
+        });
+    }
+    if (title.length > 60) {
+      return res
+        .status(400)
+        .json({ error: { message: 'Title must not exceed 60 characters' } });
+    }
+    try {
+      const post = await PostsService.insertPost(req.app.get('db'), newPost);
+      res
+        .status(201)
+        .location(path.posix.join(req.originalUrl, `${post.id}`))
+        .json(post);
+    } catch {
+      next;
+    }
+  });
+
+postsRouter.route('/:post_id').delete(requireAuth, async (req, res, next) => {
+  try {
+    const post = await PostsService.deletePost(
+      req.app.get('db'),
+      req.params.post_id
+    );
+    if (!post) {
+      return res.status(404).json({
+        error: { message: 'Post does not exist' },
+      });
+    }
+    res.status(204).end();
+  } catch {
+    next;
+  }
+});
+
+module.exports = postsRouter;
