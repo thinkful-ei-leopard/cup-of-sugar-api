f7097e553e8e86cb12b1f21faa40b5dfcaffc6de
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-21 08:41:27 -0600

    minor changes to login router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const helmet=require('helmet');
 const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router')
 
 const app= express();
 
@@ -20,6 +21,8 @@ app.get('/', (req, res) => {
         .status(200)
         .send('Hello, world!')
 });
+
+app.use('/api/auth', authRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
