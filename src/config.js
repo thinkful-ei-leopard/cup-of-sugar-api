bff712f04ae545b962a0b9e317748762d2b01488
Daniel Fong <daniel.fong.mv@gmail.com>
2020-04-21 10:37:03 -0600

    fixed expiry

--- src/config.js
+++ src/config.js
@@ -4,4 +4,5 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/cup-of-sugar',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/cup-of-sugar-test',
     JWT_SECRET: process.env.JWT_SECRET || 'its a secret',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   }
--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,35 +1,37 @@
+const { JsonWebTokenError } = require('jsonwebtoken')
 const AuthService = require('../auth/auth-service')
 
-function requireAuth(req, res, next) {
-    const authToken = req.get('Authorization') || ''
+async function requireAuth(req, res, next) {
+  const authToken = req.get('Authorization') || ''
 
-    let bearerToken
-    if (!authToken.toLowerCase().startsWith('bearer ')) {
+  let bearerToken
+  if (!authToken.toLowerCase().startsWith('bearer ')) {
     return res.status(401).json({ error: 'Missing bearer token' })
-    } else {
-      bearerToken = authToken.slice(7, authToken.length)
-    } 
-        try {
-        const payload = AuthService.verifyJwt(bearerToken)  
-          AuthService.getUserWithUserName(
-            req.app.get('db'),
-            payload.sub,
-          )
-            .then(user => {
-              if (!user)
-                return res.status(401).json({ error: 'Unauthorized request' }) 
-        req.user = user  
-        next()
-      })
-        .catch(err => {
-          console.error(err)
-          next(err)
-        })
-      } catch(error) {
-        res.status(401).json({ error: 'Unauthorized request' })
-      }
-    }
-  
-      module.exports = {
-    requireAuth,
+  } else {
+    bearerToken = authToken.slice(7, authToken.length)
   }
+
+  try {
+    const payload = AuthService.verifyJwt(bearerToken)
+
+    const user = await AuthService.getUserWithUserName(
+      req.app.get('db'),
+      payload.sub,
+    )
+
+    if (!user)
+      return res.status(401).json({ error: 'Unauthorized request' })
+
+    req.user = user
+    next()
+  } catch (error) {
+    if (error instanceof JsonWebTokenError)
+      return res.status(401).json({ error: 'Unauthorized request' })
+
+    next(error)
+  }
+}
+
+module.exports = {
+  requireAuth,
+}
