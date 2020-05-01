ec6a17bc78a174fe387e79ccf96dd40525abf198
Christopher Floyd <58857442+cfloyd252@users.noreply.github.com>
2020-05-01 09:02:52 -0700

    still working on it

--- migrations/001.do.create_users.sql
+++ migrations/001.do.create_users.sql
@@ -5,5 +5,7 @@ CREATE TABLE users (
     password TEXT NOT NULL,
     email TEXT NOT NULL,
     zip INTEGER NOT NULL,
-    admin_status TEXT NOT NULL
+    admin_status TEXT NOT NULL,
+    img_src TEXT,
+    img_alt TEXT NOT NULL
 );
